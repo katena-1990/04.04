5764b41ec3e9fe6c59a3867bc45bc21dbb08b306
Katya Panko <katena-1990@mail.ru>
2022-07-05 15:04:18 +0300

    задача1

---
+++ задача1/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень В
+
+Console.WriteLine("Введите первое число: ");
+string firstStr = Console.ReadLine();
+int firstA = int.Parse(firstStr);
+
+Console.WriteLine("Введите второе число: ");
+string secondStr = Console.ReadLine();
+int secondB = int.Parse(secondStr);
+
+int solution = 1;
+int i = 1;
+while(i <= secondB)
+{
+    solution = firstA * solution;
+    i++;
+}
+Console.WriteLine(solution);
+

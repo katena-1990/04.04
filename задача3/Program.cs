be9d99e8fbc6f95558f3504c7558edc140d95516
Katya Panko <katena-1990@mail.ru>
2022-07-05 15:47:06 +0300

    задача3

---
+++ задача3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Random rnd = new Random();
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0,9);
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}");
+}
